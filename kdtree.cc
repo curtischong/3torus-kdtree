9075c0c65692667f28dbe15231b311683124aabf
Curtis Chong <curtischong5@gmail.com>
2025-01-05 14:31:35 -0500

    gitignore and hello world

---
+++ kdtree.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+
+#include <sys/time.h>
+
+
+
+int main(int argc, char* argv[]){
+    std::cout << "hi" << std::endl;
+}
